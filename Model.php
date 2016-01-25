c19b816c1d6beb275c7cf7363a18ee9608cacd30
andela-cvundi <christophervundi@gmail.com>
2016-01-25 10:12:03 +0300

    (fix) Add CRUD methods in the Model class which the user can extend with whatever class it is that they are working with

---
+++ Model.php
@@ -0,0 +1,43 @@
+<?php
+
+class Model
+{
+    private static $db;
+    protected static $entity_table = 'Person';
+
+    public function __construct()
+    {
+        self::$db = new Database;
+    }
+
+    public function add()
+    {
+        foreach ($this->db_fields as $key) {
+            $data[$key] = $this->$key;
+        }
+        $this->db->insert($this->entity_table, $data);
+    }
+
+    public function update()
+    {
+        foreach ($this->db_fields as $key) {
+            if (!is_null($this->key)) {
+                $data[$key] = $this->$key;;
+            }
+        }
+        $where = ' ';
+        foreach ($primary_keys as $key) {
+            $where .=' '.$key ." = ".$this->$key." &&";
+        }
+
+        $where = rtrim($where, '&');
+        $this->db->update($this->entity_table, $data, $where);
+    }
+
+    public static function remove($id)
+    {
+        $where = "id = '$id'";
+        new static();
+        self::$db->delete(self::$entity_table, $where);
+    }
+}
