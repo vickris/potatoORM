f0d0af755b40aa0e2c9ebfe5d3fb3661d372e744
andela-cvundi <christophervundi@gmail.com>
2016-02-08 23:57:28 +0300

    (fix) Make database conection test pass

--- tests/DBConnectionTest.php
+++ tests/DBConnectionTest.php
@@ -3,40 +3,14 @@
 namespace Vundi\Potato\Test;
 
 use PHPUnit_Framework_TestCase;
-use PDO;
+use Vundi\Potato\Database;
 
 class DBConnectionTest extends PHPUnit_Framework_TestCase
 {
     public function testConnection()
     {
-        $connection = [
-            'host' => 'localhost',
-            'user' => 'root',
-            'pass' => '',
-            'dbname' => 'potato',
-            'dbtype' => 'mysql',
-        ];
-
-        $options = array(
-            PDO::ATTR_PERSISTENT => true,
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        );
-
-        $dsn = $connection['dbtype'].':host='.$connection['host'].';dbname='.$connection['dbname'];
-
-        $DB = new PDO($dsn, $connection['user'], $connection['pass'], $options);
-
+        new Database();
+        $DB = Database::$db_handler;
         $this->assertInstanceOf('PDO', $DB);
-    }
-
-    /**
-     * [testErrorIsThrownIfUserDoesNotProvideConnectionDetails description].
-     *
-     * @expectedException \PDOException
-     */
-    public function testErrorIsThrownIfUserDoesNotProvideConnectionDetails()
-    {
-        $dsn = ' ';
-        $DB = new PDO($dsn);
     }
 }
